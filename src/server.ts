43038685b49ad415e07174d08d019595718cdc9c
Ricky Chon <rickychon99@gmail.com>
2023-05-02 15:06:02 -0400

    Add `/chat` endpoint with streamed response

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import apiRouter from "./routes";
 
 const server = express();
 
@@ -12,9 +13,13 @@ server.use(
     skip: (req, res) => process.env.NODE_ENV === "test",
   })
 );
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
 server.get("/health", (req, res) => {
   res.status(200).send("Healthy!");
 });
+
+server.use("/api", apiRouter);
 
 export default server;
