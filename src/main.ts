d6c273f7755ea54a753dbbac7e402ea91a9d8b6b
Ricky Chon <rickychon99@gmail.com>
2023-05-02 15:07:25 -0400

    Add `/chat` endpoint with streamed response (#2)
    * Add middlewares for Express
    
    CORS, HTTP header security, HTTP request logging
    
    * Add `/chat` endpoint with streamed response

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import server from "./server";
 
 const port = 8000;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,25 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import apiRouter from "./routes";
 
 const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms", {
+    skip: (req, res) => process.env.NODE_ENV === "test",
+  })
+);
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
 server.get("/health", (req, res) => {
   res.status(200).send("Healthy!");
 });
+
+server.use("/api", apiRouter);
 
 export default server;
