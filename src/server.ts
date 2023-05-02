f90333fa69c74f7c92aa8904d672f1ca5281ce2e
Ricky Chon <rickychon99@gmail.com>
2023-05-02 13:35:08 -0400

    Add middlewares for Express
    CORS, HTTP header security, HTTP request logging

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms", {
+    skip: (req, res) => process.env.NODE_ENV === "test",
+  })
+);
 
 server.get("/health", (req, res) => {
   res.status(200).send("Healthy!");
